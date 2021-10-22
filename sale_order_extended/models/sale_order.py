3d6ad7d98a5096dc09637c3de38cde62eab8483f
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-22 18:34:44 +0530

    [ADD] Emipro Training || Odoo Exercise - 2 Modified_22/10/2021_(Added Some code in sale_order_extended module)

--- sale_order_extended/models/sale_order.py
+++ sale_order_extended/models/sale_order.py
@@ -9,10 +9,18 @@ class SaleOrderExtended(models.Model):
     def action_confirm(self):
         product = self.env.ref('sale_order_extended.product_data')
 
+        # order_line = self.env['sale.order.line'].create(
+        #     {
+        #         'product_id': product.id,
+        #         'product_uom_qty': 1,
+        #         'price_unit': product.price,
+        #     })
+
         self.order_line = [(0, 0,
                             {
                                 'product_id': product.id,
                                 'product_uom_qty': 1,
                                 'price_unit': product.price,
                             })]
+
         return super(SaleOrderExtended, self).action_confirm()
