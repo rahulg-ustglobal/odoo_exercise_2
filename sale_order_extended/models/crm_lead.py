f599a806dfee6dcf54848f041d737ad73aac3aff
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-22 12:02:05 +0530

    [ADD] Emipro Training || Odoo Exercise - 2 Modified_22/10/2021_(Added Some code in sale_order_extended module)

--- sale_order_extended/models/crm_lead.py
+++ sale_order_extended/models/crm_lead.py
@@ -7,7 +7,8 @@ class CrmLeadExtended(models.Model):
     def action_new_quotation(self):
         quotation = super(CrmLeadExtended,self).action_new_quotation()
         category_lead = self.env.ref('sale_order_extended.tag_from_lead')
-        quotation['context']['default_tag_ids'].extend(category_lead.ids)
         quotation['context']['default_lead_id'] = self.id
+        quotation['context']['default_tag_ids'].extend(category_lead.ids)
+
         return quotation
 
--- sale_order_extended/models/sale_order.py
+++ sale_order_extended/models/sale_order.py
@@ -4,5 +4,4 @@ from odoo import fields, models, api
 class SaleOrderExtended(models.Model):
     _inherit = 'sale.order'
 
-    lead_id = fields.Many2one(comodel_name='crm.lead', string="Opportunity Lead")
-
+    lead_id = fields.Many2one(comodel_name='crm.lead', string="New Opportunity")
