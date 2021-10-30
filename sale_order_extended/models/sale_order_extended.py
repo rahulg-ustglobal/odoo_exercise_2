1eaf1fdcd607dd26f75e36f906be6fddb7701d41
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-30 11:10:55 +0530

    [ADD] Emipro Training || Odoo Exercise - 2 Modified_30/10/2021_(Added Some of the code in sale_order_extended module)

--- sale_order_extended/models/sale_order_extended.py
+++ sale_order_extended/models/sale_order_extended.py
@@ -40,11 +40,8 @@ class SaleOrderExtended(models.Model):
                 [('product_id', '=', order_line.product_id.id), ('order_id', '!=', self.id)])
         move_ids = sale_order_lines.move_ids
 
-        # move_ids = sale_order_lines.move_ids.filtered(
-        #     lambda move: move.forecast_availability > 0 and move.state not in ['done', 'cancel'])
-        
-        for moves in move_ids:
-            lambda move: move.forecast_availability > 0 and move.state not in ['done', 'cancel']
+        move_ids = sale_order_lines.move_ids.filtered(
+            lambda move: move.forecast_availability > 0 and move.state not in ['done', 'cancel'])
 
         sale_order_line_ids = move_ids.sale_line_id.ids
 
@@ -57,30 +54,5 @@ class SaleOrderExtended(models.Model):
         }
         return action
 
-    def _confirm_validate_btn(self):
-        for order in self:
-            if any(expense_policy not in [False, 'no'] for expense_policy in
-                   order.order_line.mapped('product_id.expense_policy')):
-                if not order.analytic_account_id:
-                    order._create_analytic_account()
-        return True
-
     def confirm_validate_btn(self):
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
-
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write(self._prepare_confirmation_values())
-
-        # Context key 'default_name' is sometimes propagated up to here.
-        # We don't need it and it creates issues in the creation of linked records.
-        context = self._context.copy()
-        context.pop('default_name', None)
-
-        self.with_context(context)._confirm_validate_btn()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-        return True
+        pass
