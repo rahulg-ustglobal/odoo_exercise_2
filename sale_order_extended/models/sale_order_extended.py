fc8f60d0f5e268ebcc336c3b744aebcbf1a9e116
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-30 10:34:02 +0530

    [ADD] Emipro Training || Odoo Exercise - 2 Modified_30/10/2021_(Added Some of the code in sale_order_extended module)

--- sale_order_extended/models/sale_order_extended.py
+++ sale_order_extended/models/sale_order_extended.py
@@ -1,6 +1,5 @@
-from odoo import fields, models, api,_
+from odoo import fields, models, api, _
 from odoo.exceptions import UserError, ValidationError
-
 
 
 class SaleOrderExtended(models.Model):
@@ -9,37 +8,17 @@ class SaleOrderExtended(models.Model):
     lead_id = fields.Many2one(comodel_name='crm.lead', string="New Opportunity")
 
     def action_confirm(self):
-        product = self.env.ref('sale_order_extended.product_data')
-
-        # order_line = self.env['sale.order.line'].create(
-        #     {
-        #         'product_id': product.id,
-        #         'product_uom_qty': 1,
-        #         'price_unit': product.price,
-        #     })
-
-        self.order_line = [(0, 0,
-                            {
-                                'product_id': product.id,
-                                'product_uom_qty': 1,
-                                'price_unit': product.price,
-                            })]
+        # product = self.env.ref('sale_order_extended.product_data')
+        # self.order_line = [(0, 0,
+        #                     {
+        #                         'product_id': product.id,
+        #                         'product_uom_qty': 1,
+        #                         'price_unit': product.price,
+        #                     })]
 
         return super(SaleOrderExtended, self).action_confirm()
 
     def manage_deposits_btn(self):
-        # for order_line in self.order_line:
-        #     if order_line.product_id.deposit_product_id:
-        #         deposit_product = order_line.product_id.deposit_product_id
-        #         sale_order_line = self.env['sale.order.line'].write({
-        #                                     'product_id': deposit_product.id,
-        #                                     'product_uom_qty': order_line.product_uom_qty * order_line.product_id.deposit_product_qty,
-        #                                     'price_unit': deposit_product.lst_price,
-        #                                     'product_uom': deposit_product.uom_id
-        #         })
-        #      else:
-        #           raise ValidationError("You can't create deposit product one more time..!!!")
-
         for order_line in self.order_line:
             if order_line.product_id.deposit_product_id:
                 if order_line.child_ids:
@@ -57,17 +36,31 @@ class SaleOrderExtended(models.Model):
 
     def manage_products_btn(self):
         for order_line in self.order_line:
-            if order_line not in self.order_line.product_id.ids:
-                order_line.other_sale_order_lines = self.env['sale.order.line'].search([('state','=','Done,Cancel'),
-                                                                             ('product_uom_qty','>',0)])
+            sale_order_lines = self.order_line.search(
+                [('product_id', '=', order_line.product_id.id), ('order_id', '!=', self.id)])
+        move_ids = sale_order_lines.move_ids
 
-    #other_sale_order_lines.mapped(lambda line: line.state)
+        # move_ids = sale_order_lines.move_ids.filtered(
+        #     lambda move: move.forecast_availability > 0 and move.state not in ['done', 'cancel'])
+        
+        for moves in move_ids:
+            lambda move: move.forecast_availability > 0 and move.state not in ['done', 'cancel']
 
+        sale_order_line_ids = move_ids.sale_line_id.ids
 
+        action = {
+            'name': _('Tree of Products'),
+            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', sale_order_line_ids)],
+            'view_mode': 'tree',
+            'res_model': 'sale.order.line',
+        }
+        return action
 
     def _confirm_validate_btn(self):
         for order in self:
-            if any(expense_policy not in [False, 'no'] for expense_policy in order.order_line.mapped('product_id.expense_policy')):
+            if any(expense_policy not in [False, 'no'] for expense_policy in
+                   order.order_line.mapped('product_id.expense_policy')):
                 if not order.analytic_account_id:
                     order._create_analytic_account()
         return True
