ee4b60542d099a0be0ba75aa9cb41bf4c867b586
Rahul Ghanghave <rahulg@emiprotechnologies.com>
2021-10-30 09:05:28 +0530

    [ADD] Emipro Training || Odoo Exercise - 2 Modified_30/10/2021_(Added Some of the code in sale_order_extended module)

--- sale_order_extended/models/sale_order_extended.py
+++ sale_order_extended/models/sale_order_extended.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models, api,_
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -42,8 +42,8 @@ class SaleOrderExtended(models.Model):
 
         for order_line in self.order_line:
             if order_line.product_id.deposit_product_id:
-                if order_line.child_id:
-                    order_line.child_id.product_uom_qty = order_line.product_uom_qty * order_line.product_id.deposit_product_qty
+                if order_line.child_ids:
+                    order_line.child_ids.product_uom_qty = order_line.product_uom_qty * order_line.product_id.deposit_product_qty
                 else:
                     deposit_product = order_line.product_id.deposit_product_id
                     sale_order_line_list = [(0, 0, {
@@ -55,7 +55,39 @@ class SaleOrderExtended(models.Model):
                     })]
                     self.order_line = sale_order_line_list
 
-    def get_products_btn(self):
+    def manage_products_btn(self):
         for order_line in self.order_line:
-            current_products = order_line.product_id
-            other_sale_order_lines = self.env['sale.order'].search(['current_product','!=',''])
+            if order_line not in self.order_line.product_id.ids:
+                order_line.other_sale_order_lines = self.env['sale.order.line'].search([('state','=','Done,Cancel'),
+                                                                             ('product_uom_qty','>',0)])
+
+    #other_sale_order_lines.mapped(lambda line: line.state)
+
+
+
+    def _confirm_validate_btn(self):
+        for order in self:
+            if any(expense_policy not in [False, 'no'] for expense_policy in order.order_line.mapped('product_id.expense_policy')):
+                if not order.analytic_account_id:
+                    order._create_analytic_account()
+        return True
+
+    def confirm_validate_btn(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        self.write(self._prepare_confirmation_values())
+
+        # Context key 'default_name' is sometimes propagated up to here.
+        # We don't need it and it creates issues in the creation of linked records.
+        context = self._context.copy()
+        context.pop('default_name', None)
+
+        self.with_context(context)._confirm_validate_btn()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+        return True
--- sale_order_extended/models/sale_order_line_extended.py
+++ sale_order_extended/models/sale_order_line_extended.py
@@ -5,4 +5,4 @@ class SaleOrderLineExtended(models.Model):
     _inherit = "sale.order.line"
 
     parent_id = fields.Many2one(comodel_name='sale.order.line')  # order_line
-    child_id = fields.One2many('sale.order.line', 'parent_id')  # deposit_product
+    child_ids = fields.One2many('sale.order.line', 'parent_id')  # deposit_product
